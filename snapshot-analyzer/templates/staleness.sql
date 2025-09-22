39f89bf4b2b3cbf116d74dbff6a2fc940ac8363f
igor56D <igor.durovic@anza.xyz>
2025-09-22 16:54:27 -0600

    fix null results

--- snapshot-analyzer/templates/staleness.sql
+++ snapshot-analyzer/templates/staleness.sql
@@ -7,11 +7,12 @@
 SELECT 
     COUNT(*) as account_count,
     SUM(account_size) as total_bytes,
-    ROUND(CAST(SUM(account_size) AS REAL) / 1000000000.0, 2) as total_gb,
+    ROUND(SUM(account_size) * 1.0 / 1000000.0, 2) as total_mb,
+    ROUND(SUM(account_size) * 1.0 / 1000000000.0, 4) as total_gb,
     SUM(lamports) as total_lamports,
-    ROUND(CAST(SUM(lamports) AS REAL) / 1000000000.0, 2) as total_sol,
+    ROUND(SUM(lamports) * 1.0 / 1000000000.0, 2) as total_sol,
     SUM(executable) as executable_accounts,
-    AVG(account_size) as avg_account_size,
-    AVG(lamports) as avg_lamports
+    ROUND(AVG(account_size * 1.0), 0) as avg_account_size,
+    ROUND(AVG(lamports * 1.0), 0) as avg_lamports
 FROM accounts 
 WHERE MAX(max_read_epoch, max_write_epoch) >= {{current_epoch}} - {{lookback_epochs}};
